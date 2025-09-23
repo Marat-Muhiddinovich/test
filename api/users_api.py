32860ee5ead517ed9e02ffd2a0cb4d77b081d4e3
Marat-Muhiddinovich <maratqochqorov46@gmail.com>
2025-09-23 13:52:46 +0500

    add login api

--- api/users_api.py
+++ api/users_api.py
@@ -6,4 +6,17 @@ class UsersAPI(APIClient):
         self.base_endpoint = "/users"   
 
     def get_all_users(self):
-        return self.get(self.base_endpoint)    
+        return self.get(self.base_endpoint) 
+
+class LoginAPI(APIClient):
+    def __init__(self, context):
+        super().__init__(context)
+        self.base_endpoint = "/auth/login"
+
+    def login(self, username: str, password: str, expiresInMins: int = 60):
+        payload = {
+            "username": username,
+            "password": password,
+            "expiresInMins": expiresInMins
+        }   
+        return self.post(self.base_endpoint, data=payload)
