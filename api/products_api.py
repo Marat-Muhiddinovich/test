ca03e93353612c9e88136d34bb20819677f69b24
Marat-Muhiddinovich <maratqochqorov46@gmail.com>
2025-10-03 14:38:30 +0500

    add feautures

--- api/products_api.py
+++ api/products_api.py
@@ -9,6 +9,7 @@ class ProductsAPI(APIClient):
     def get_all_products(self):
         return self.get(f"{self.base_endpoint}")
     
+# get product by ID
 class ProductByIDAPI(APIClient):
     def __init__(self, context):
         super().__init__(context)
@@ -18,3 +19,50 @@ class ProductByIDAPI(APIClient):
         if not isinstance(product_id, int) or product_id < 0:
             raise ValueError("product_id must be a positive integer")
         return self.get(f"{self.base_endpoint}/{product_id}")
+# search products
+class SearchProductsAPI(APIClient):
+    def __init__(self, context):
+        super().__init__(context)
+        self.base_endpoint = "/products"
+    
+    def search_products(self, search_item=str):
+        if not isinstance(search_item, str) or not search_item.strip():
+            raise ValueError("search_item must be a non-empty string")
+        return self.get(f"{self.base_endpoint}/search?q={search_item}")
+    
+# limit products
+class LimitProductsAPI(APIClient):
+    def __init__(self, context):
+        super().__init__(context)
+        self.base_endpoint = "/products"
+
+    def limit_products(self, limit=int, skip=int, select=str):
+        if not isinstance(limit, int) or limit <= 0:
+            raise ValueError("limit must be a positive integer")
+        if not isinstance(skip, int) or skip <= 0:
+            raise ValueError("skip must be a positive integer")
+        if not isinstance(select, str) or not select.strip():
+            raise ValueError("select must be a non-empty string")
+        return self.get(f"{self.base_endpoint}?limit={limit}&skip={skip}&select={select}")
+    
+# sort products
+class SortProductsAPI(APIClient):
+    def __init__(self, context):
+        super().__init__(context)
+        self.base_endpoint = "/products"
+    
+    def sort_products(self, sort_by=str):
+        if not isinstance(sort_by, str) or not sort_by.strip():
+            raise ValueError("sort_by must be a non-empty string")
+        return self.get(f"{self.base_endpoint}?ordering={sort_by}")
+    
+#categories of products
+class CategoriesAPI(APIClient):
+    def __init__(self, context):
+        super().__init__(context)
+        self.base_endpoint = "/products/category"
+
+    def get_single_category(self, category_name=str):
+        if not isinstance(category_name, str) or not category_name.strip():
+            raise ValueError("category_name must be a non-empty string")
+        return self.get(f"{self.base_endpoint}/{category_name}")
